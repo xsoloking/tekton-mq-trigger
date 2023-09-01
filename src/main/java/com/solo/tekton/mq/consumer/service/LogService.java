b45cce220c56b7376bc12a20676dc78e1b62b047
soloking <soloking.x@gmail.com>
2023-09-01 22:12:02 +0800

    v.02

--- src/main/java/com/solo/tekton/mq/consumer/service/LogService.java
+++ src/main/java/com/solo/tekton/mq/consumer/service/LogService.java
@@ -4,6 +4,7 @@ import com.solo.tekton.mq.consumer.data.TaskLog;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.LogWatch;
 import io.fabric8.kubernetes.client.dsl.PodResource;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +12,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
@@ -34,20 +34,27 @@ public class LogService {
 
     private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
+    /**
+     * Redirects logs for a task.
+     *
+     * @param taskLog the TaskLog object containing the task instance ID
+     * @return void
+     */
     @Async
     public void redirectLogs(TaskLog taskLog) {
-        log.info("Redirecting logs for task \"{}\"",  taskLog.getTaskInstanceId());
+        log.info("Redirecting logs for task \"{}\"", taskLog.getTaskInstanceId());
         try {
             Map<String, String> labelFilter = Map.of(
-                    "devops.flow/taskInstanceId",
-                    String.valueOf(taskLog.getTaskInstanceId()),
-                    "tekton.dev/pipelineTask","main");
+                    "devops.flow/taskInstanceId", String.valueOf(taskLog.getTaskInstanceId()),
+                    "tekton.dev/pipelineTask", "main");
             Optional<PodResource> podRes = kubernetesClient.pods()
                     .inNamespace(namespace)
                     .withLabels(labelFilter)
                     .resources()
                     .findFirst();
             if (podRes.isEmpty()) {
+                taskLog.setLogContent("ERROR::no pod found for taskInstance: " + taskLog.getTaskInstanceId());
+                insertLogToMongo(taskLog);
                 log.info("no pod found for taskInstance: {}, log redirection was cancelled", taskLog.getTaskInstanceId());
                 return;
             }
@@ -56,22 +63,25 @@ public class LogService {
 //                log.info("Task \"{}\" doesn't start to run in {} minutes, log redirection was cancelled", taskLog.getTaskInstanceId());
 //                return;
 //            }
-            LogWatch watch= podRes.get().watchLog(new OutputStream() {
+            LogWatch watch = podRes.get().watchLog(new OutputStream() {
                 @Override
-                public void write(int b) throws IOException {
+                public void write(int b) {
                     throw new RuntimeException("not used");
                 }
+
                 @Override
-                public void write(byte[] b, int off, int len) throws IOException {
-                    for (String line: new String(b, off, len).trim().split("\n")) {
+                public void write(byte @NonNull [] b, int off, int len) {
+                    for (String line : new String(b, off, len).trim().split("\n")) {
                         taskLog.setLogContent(line);
                         insertLogToMongo(taskLog);
                     }
                 }
             });
-            podRes.get().waitUntilCondition(  r -> r.getStatus().getPhase().equals("Succeeded")
+            podRes.get().waitUntilCondition(r -> r.getStatus().getPhase().equals("Succeeded")
                     || r.getStatus().getPhase().equals("Terminated"), taskLog.getTimeout(), TimeUnit.MINUTES);
         } catch (Exception e) {
+            taskLog.setLogContent("An exception happened during log redirection: " + e.getMessage());
+            insertLogToMongo(taskLog);
             log.error("Redirecting logs for task \"{}\" was failed due to {}", taskLog.getTaskInstanceId(), e.getMessage());
         }
     }
@@ -86,7 +96,7 @@ public class LogService {
         String timestamp = sdf.format(new Timestamp(System.currentTimeMillis()));
         String msg = timestamp + " [INFO] " + taskLog.getLogContent();
         newTaskLog.setLogType(2);
-        if(taskLog.getLogContent().toLowerCase().contains("error") || taskLog.getLogContent().toLowerCase().contains("exception")) {
+        if (taskLog.getLogContent().contains("ERROR") || taskLog.getLogContent().toLowerCase().contains("exception")) {
             newTaskLog.setLogType(1);
             msg = timestamp + " [ERROR] " + taskLog.getLogContent();
         } else if (taskLog.getLogContent().toLowerCase().contains("warning")) {
